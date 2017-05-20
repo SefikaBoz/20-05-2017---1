ee6302db33897a795da1b7afcf934c22ecb3cd5d
SefikaBoz <sefikaboz12@gmail.com>
2017-05-20 16:01:39 +0300

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+# 20-05-2017---1
+
+#define DEBUG
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Attribute
+{
+    class Class1
+    {
+        [Conditional ("DEBUG")]
+        public void M()
+        {
+            Console.WriteLine("Execute Class1.M");
+        }
+    }
+   
+}
