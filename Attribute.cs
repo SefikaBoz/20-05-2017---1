0be300572a668e9f6f19c9632bc3e4d1cbfd3a3a
SefikaBoz <sefikaboz12@gmail.com>
2017-05-20 16:00:57 +0300

    Create Attribute.cs

---
+++ Attribute.cs
@@ -0,0 +1,29 @@
+# 20-05-2017---1
+
+#define TURKCE
+#define ENGLISH
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Attribute
+{
+    class Attribute
+    {
+        [Conditional("TURKCE")]
+        public void Turkce()
+        {
+            Console.WriteLine("Merhaba Dünya");
+        }
+        [Conditional("ENGLISH")]
+        public void English()
+        {
+            Console.WriteLine("Hello World");
+        }
+       
+
+    }
+}
